e623035df319e19ef03907e7a9108d899c1a9341
ArmandoVallejo <juanarmandovh20@gmail.com>
2024-09-17 20:13:45 -0600

    se agrega la parte de albumes y estados de animo

--- app/src/main/java/com/example/interfazytm/MainActivity.kt
+++ app/src/main/java/com/example/interfazytm/MainActivity.kt
@@ -10,16 +10,23 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -31,6 +38,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -48,6 +56,8 @@ class MainActivity : ComponentActivity() {
             ){
                 TopBar()
                 TopElements()
+                AlbumesSencillos()
+                AnimoGeneros()
             }
             
         }
@@ -76,7 +86,8 @@ fun GreetingPreview() {
 fun TopBar() {
     Row(
         modifier = Modifier
-            .fillMaxWidth(),
+            .fillMaxWidth()
+            .padding(start = 10.dp, top = 10.dp, end = 10.dp),
         horizontalArrangement = Arrangement.SpaceBetween // Distribuye los elementos entre los extremos
     ) {
         Row(
@@ -91,6 +102,7 @@ fun TopBar() {
             )
             Text(
                 text = "Music",
+                fontWeight = FontWeight.Bold,
                 color = Color.White,
                 modifier = Modifier
                     .padding(top = 1.dp, start = 2.dp)
@@ -128,7 +140,7 @@ fun TopElements(){
         modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp)
-            .height(170.dp)
+            .height(110.dp)
     ){
         Column (
             modifier= Modifier
@@ -179,7 +191,7 @@ fun TopElements(){
             modifier= Modifier
                 .clip(RoundedCornerShape(3.dp))
                 .background(color = colorResource(R.color.topelementsgray))
-                .align(Alignment.CenterStart)
+                .align(Alignment.BottomStart)
                 .width(170.dp)
                 .padding(10.dp)
         ){
@@ -202,7 +214,7 @@ fun TopElements(){
             modifier= Modifier
                 .clip(RoundedCornerShape(3.dp))
                 .background(color = colorResource(R.color.topelementsgray))
-                .align(Alignment.CenterEnd)
+                .align(Alignment.BottomEnd)
                 .width(180.dp)
                 .padding(10.dp)
         ){
@@ -223,5 +235,336 @@ fun TopElements(){
 
     }
 }
+
+@Preview
+@Composable
+fun AlbumesSencillos(){
+    Column(
+    ){
+        Row (
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = "Albumes y sencillos nuevos",
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = "Arrow Right",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(20.dp)
+            )
+        }
+        Spacer(modifier = Modifier.size(10.dp))
+
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+
+        ){
+            Column(
+            ){
+                Image(
+                    painter = painterResource(R.drawable.thenewabnormal),
+                    contentDescription = "The New Abnormal Cover",
+                    modifier= Modifier
+                        .padding(start = 10.dp)
+                        .clip(RoundedCornerShape(5.dp))
+                        .size(90.dp)
+                )
+                Text(
+                    text = "The New Abnormal",
+                    color=Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 5.dp),
+                )
+                Text(
+                    text = "Album • The Strokes",
+                    color= colorResource(R.color.albumartist),
+                    fontWeight = FontWeight.Light,
+                    fontSize = 9.sp,
+                    modifier = Modifier
+                        .padding(start=10.dp)
+                )
+
+            }
+
+            Column(
+            ){
+                Image(
+                    painter = painterResource(R.drawable.elcirculo),
+                    contentDescription = "El Circulo Cover",
+                    modifier= Modifier
+                        .padding(start = 10.dp)
+                        .clip(RoundedCornerShape(5.dp))
+                        .size(90.dp)
+                )
+                Text(
+                    text = "El Circulo",
+                    color=Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 5.dp),
+                )
+                Text(
+                    text = "Album • Kase.O",
+                    color= colorResource(R.color.albumartist),
+                    fontWeight = FontWeight.Light,
+                    fontSize = 9.sp,
+                    modifier = Modifier
+                        .padding(start=10.dp)
+                )
+
+            }
+            Column(
+            ){
+                Image(
+                    painter = painterResource(R.drawable.mrmorale),
+                    contentDescription = "Mr. Morale and The Big Steppers Cover",
+                    modifier= Modifier
+                        .padding(start = 10.dp)
+                        .clip(RoundedCornerShape(5.dp))
+                        .size(90.dp)
+                )
+                Text(
+                    text = "Mr. Morale & The Big Steppers",
+                    color=Color.White,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 10.sp,
+                    modifier = Modifier
+                        .padding(start = 10.dp, top = 5.dp),
+                )
+                Text(
+                    text = "Album • Kendrick Lamar",
+                    color= colorResource(R.color.albumartist),
+                    fontWeight = FontWeight.Light,
+                    fontSize = 9.sp,
+                    modifier = Modifier
+                        .padding(start=10.dp)
+                )
+
+            }
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AnimoGeneros(){
+    //Para que el texto quede arriba de la fila
+    Column (){
+        Row (
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ){
+            Text(
+                text = "Albumes y sencillos nuevos",
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            )
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowRight,
+                contentDescription = "Arrow Right",
+                tint = Color.White,
+                modifier = Modifier
+                    .size(20.dp)
+            )
+        }
+        LazyRow {
+            items(1) {
+                //Primera columna de elementos
+                Column (modifier = Modifier
+                    .padding(start = 20.dp)
+                    .height(110.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ){
+                    //Elemento Hip-Hop
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Green)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Hip-Hop", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                    //Elemento Para sentirse bien
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Red)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Para sentirse bien", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                    //Elemento Rock en español
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Yellow)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Rock en español", color = Color.White, fontSize = 12.sp,modifier = Modifier.padding(5.dp))
+                    }
+                }
+                //Segunda columna de elementos
+                Column (modifier = Modifier
+                    .padding(start = 20.dp)
+                    .height(110.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ){
+                    //Elemento Relajación
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Blue)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Relajación", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                    //Elemento Para el Camino
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Cyan)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Para el camino", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                    //Elemento Ejercicio
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = colorResource(R.color.purple_200))
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Ejercicio", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                }
+                //Tercer columna de elementos
+                Column (modifier = Modifier
+                    .padding(start = 20.dp)
+                    .height(110.dp),
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ){
+                    //Elemento Felicidad
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Yellow)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Felicidad", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                    //Elemento Chill
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Magenta)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Chill", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                    //Elemento Jazz
+                    Row (
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(3.dp))
+                            .background(color = Color.DarkGray)
+                            .width(130.dp)
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .background(color = Color.Red)
+                                .size(3.dp, 27.dp)
+                                .fillMaxHeight()
+                        ){
+
+                        }
+                        Text(text = "Jazz", fontSize = 12.sp, color = Color.White, modifier = Modifier.padding(5.dp))
+                    }
+                }
+            }
+        }
+
+    }
+
+    }
+
+
 
 
